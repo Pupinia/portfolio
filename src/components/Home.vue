afce01aea1f63a8b468ca25ed52ba81bd56d0360
Ivan Gorovoy <ivangorovoi1@gmail.com>
2019-05-29 17:33:22 +0700

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <div class="columns">
+      <div class="column is-three-fifths is-offset-one-fifth">
+        <transition appear appear-active-class="fade-enter-active">
+          <div class="home__content">
+            <h1 class="title home__title">Привет! Меня зовут Горовой Иван - я Front-End разработчик.</h1>
+            <div
+              class="subtitle home__text"
+            >Я программист самоучка, увлечённый современными технологиями и обожаю узнавать новое. Верю что хороший разработчик - это постоянное обучение, сложные проекты и работа над обратной свзяью, что бы стать ещё лучше.</div>
+            <button class="button is-primary" @click="$router.push({name: 'projects'})">Мои работы</button>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100%;
+  font-family: "Open Sans";
+}
+.home__content {
+  text-align: left;
+}
+.home__title {
+  margin-bottom: 10px;
+  margin-bottom: 35px !important;
+  line-height: 45px;
+}
+.fade-enter-active {
+  animation: go 1s;
+}
+
+@keyframes go {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="projects">
+    <div class="columns">
+      <div class="column">
+        <div class="column is-1">
+          <button class="button is-link" @click="$router.push({name: 'home'})">Назад</button>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <div class="project">
+          <div class="card">
+            <div class="card-content">
+              <div class="title">Hookah-Mix</div>
+              <div class="content">Приложения для хранения рецептов миксов для кальяна</div>
+            </div>
+            <footer class="card-footer">
+              <div class="card-footer-item">
+                <a href="https://hookah-mixes-18eb3.firebaseapp.com/" target="_blank">Сайт</a>
+              </div>
+              <div class="card-footer-item">
+                <a href="https://github.com/Pupinia/hookah-mix-app" target="_blank">Репозиторий</a>
+              </div>
+            </footer>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="project"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.projects {
+  margin-top: 20px;
+}
+</style>
